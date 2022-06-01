08c410cb435002cd739ffed2ae997d15788bf0f0
SamaraBilby <samarabilby@hotmail.com>
2022-06-01 10:58:45 -0400

    atualização thema 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const body = document.querySelector('body')
+const topo = document.getElementById('c-topo')
+const input = document.getElementById('c-input')
 const toggle = document.getElementById('c-toggle');
+const button =document.getElementById('c-button')
 
 toggle.onclick = function() {
     toggle.classList.toggle('active')
     body.classList.toggle('active')
+    topo.classList.toggle('active')
+    input.classList.toggle('active')
+    button.classList.toggle('active')
 }
